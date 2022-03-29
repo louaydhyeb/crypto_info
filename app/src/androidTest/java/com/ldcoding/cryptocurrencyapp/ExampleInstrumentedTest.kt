d5b698c49111aee1761aa809bda849028d0ab27b
louaydhyeb <dhyeblouay@gmail.com>
2022-03-29 13:04:05 +0200

    change app icon add screenshots

--- app/src/androidTest/java/com/ldcoding/cryptocurrencyapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/ldcoding/cryptocurrencyapp/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.ldcoding.cryptocurrencyapp", appContext.packageName)
+        assertEquals("com.plcoding.cryptocurrencyappyt", appContext.packageName)
     }
 }
